38576fd693dee75e0ad0bc410745711ba37ff071
cheeseTotopos <hshernandez406@gmail.com>
2024-05-25 14:12:55 -0600

    min 1:12:10 App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,18 @@
 import react from "react"
+import {BrowserRouter, Routes, Route, Navigation} from "react-router-dom"
+import Login from "./pages/Login"
+import Home from "./pages/Home" 
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout(){
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
 
 function App() {
+
   return (
     <>
   
